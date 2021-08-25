ff202f783fc632649d5384f1d445bfe96b2ea47f
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-08-25 21:34:55 +0100

    setting up basics

---
+++ src/data/siteconfig.js
@@ -0,0 +1,25 @@
+const siteData = {
+  title: "Ed Johnson-Williams",
+  url: "https://edjohnsonwilliams.co.uk",
+  description: "Ed Johnson-Williams' website",
+  repository: "edjw/edjw-nextjs-blog",
+  contentLicence: {
+    link: "https://creativecommons.org/licenses/by/4.0",
+    text: "Creative Commons Attribution"
+  },
+  author: {
+    name: "Ed Johnson-Williams",
+    githubUsername: "edjw",
+    twitterUsername: "@_edjw",
+    mastodonUsername: "@_edjw",
+    microblogUsername: "edjw",
+    instagramUsername: "_edjw"
+  },
+  indieauth: true,
+  micropub: true,
+  netlifyCMS: true,
+  RSSFeed: true,
+  sitemap: true
+}
+
+export default siteData
