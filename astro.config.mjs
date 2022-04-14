115df661ed0b775655fe100b8a71b2d208fb0c81
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-04-14 22:51:50 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,14 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-// export default /** @type {import('astro').AstroUserConfig} */ ({
-//
-//
-//   // devOptions: {
-//   //   tailwindConfig: "./tailwind.config.js"
-//   // }
-// });
-
 import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
-
   markdown: {
-    remarkPlugins: [
-      "remark-html",
-      "remark-gfm",
-      ["remark-footnotes", { inlineNotes: true }],
-    ],
+    remarkPlugins: ["remark-html", "remark-gfm", ["remark-footnotes", {
+      inlineNotes: true
+    }]]
   },
+  integrations: [sitemap()]
 });
