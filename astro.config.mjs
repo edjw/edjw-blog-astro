c80e868418ff9c24f2967afd08b79eb0ef77e401
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-04-14 22:53:19 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
+
   markdown: {
-    remarkPlugins: ["remark-html", "remark-gfm", ["remark-footnotes", {
-      inlineNotes: true
-    }]]
+    remarkPlugins: [
+      "remark-html",
+      "remark-gfm",
+      ["remark-footnotes", { inlineNotes: true }],
+    ],
   },
   integrations: [sitemap()]
 });
