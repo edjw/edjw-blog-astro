1cc1213da8da0c33c35b7d4d585c186876a469f9
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-04-14 23:09:01 +0100

    add astro adaptors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
-
   markdown: {
-    remarkPlugins: [
-      "remark-html",
-      "remark-gfm",
-      ["remark-footnotes", { inlineNotes: true }],
-    ],
+    remarkPlugins: ["remark-html", "remark-gfm", ["remark-footnotes", {
+      inlineNotes: true
+    }]]
   },
-  integrations: [sitemap()]
+  integrations: [sitemap(), tailwind(), netlify()]
 });
