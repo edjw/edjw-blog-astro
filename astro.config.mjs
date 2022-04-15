b00cc469551df88e42de66dd326d70e16a441d27
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-04-15 09:54:27 +0100

    remove netlify from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +17,5 @@ export default defineConfig({
       ],
     ],
   },
-  integrations: [sitemap(), tailwind(), netlify()],
+  integrations: [sitemap(), tailwind()],
 });
