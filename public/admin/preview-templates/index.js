f70b13bbc2e150c7fc201a9e5cf2733a1653c4e8
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-07-02 21:17:17 +0100

    reformatted admin pages

--- public/admin/preview-templates/index.js
+++ public/admin/preview-templates/index.js
@@ -1,5 +1,5 @@
 import Post from "/admin/preview-templates/post.js";
-import Page from "/admin/preview-templates/page.js";
+import Page from "/admin/preview-templatEes/page.js";
 
 // Register the Post component as the preview for entries in the blog collection
 CMS.registerPreviewTemplate("blog", Post);
@@ -8,13 +8,13 @@ CMS.registerPreviewTemplate("pages", Page);
 // CMS.registerPreviewStyle("/assets/css/main.css");
 // Register any CSS file on the home page as a preview style
 fetch("/")
-.then((response) => response.text())
-.then((html) => {
-const f = document.createElement("html");
-f.innerHTML = html;
-Array.from(f.getElementsByTagName("link")).forEach((tag) => {
-if (tag.rel == "stylesheet" && !tag.media) {
-CMS.registerPreviewStyle(tag.href);
-}
-});
-});
+    .then((response) => response.text())
+    .then((html) => {
+        const f = document.createElement("html");
+        f.innerHTML = html;
+        Array.from(f.getElementsByTagName("link")).forEach((tag) => {
+            if (tag.rel == "stylesheet" && !tag.media) {
+                CMS.registerPreviewStyle(tag.href);
+            }
+        });
+    });
