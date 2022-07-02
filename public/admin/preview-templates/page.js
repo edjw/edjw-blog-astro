9dafe092da0d05809ccfa702ebd3cc3a5f385db2
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-07-02 21:03:21 +0100

    revert astro-netlify-cms

---
+++ public/admin/preview-templates/page.js
@@ -0,0 +1,22 @@
+import htm from "https://unpkg.com/htm?module";
+
+const html = htm.bind(h);
+
+// Preview component for a Page
+const Page = createClass({
+    render() {
+        const entry = this.props.entry;
+
+        return html`
+      <main>
+        <article class="prose">
+          <h2>${entry.getIn(["data", "title"], null)}</h1>
+
+          ${this.props.widgetFor("body")}
+        </article>
+      </main>
+    `;
+    },
+});
+
+export default Page;
