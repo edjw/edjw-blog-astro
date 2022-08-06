6f6a658dfb90f004d822bb83c5f36b930cd5f63d
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2022-08-06 22:13:25 +0100

    update to v1rc so changing to mdx and all sorts

--- public/admin/preview-templates/index.js
+++ public/admin/preview-templates/index.js
@@ -8,13 +8,13 @@ CMS.registerPreviewTemplate("pages", Page);
 // CMS.registerPreviewStyle("/assets/css/main.css");
 // Register any CSS file on the home page as a preview style
 fetch("/")
-    .then((response) => response.text())
-    .then((html) => {
-        const f = document.createElement("html");
-        f.innerHTML = html;
-        Array.from(f.getElementsByTagName("link")).forEach((tag) => {
-            if (tag.rel == "stylesheet" && !tag.media) {
-                CMS.registerPreviewStyle(tag.href);
-            }
-        });
+  .then((response) => response.text())
+  .then((html) => {
+    const f = document.createElement("html");
+    f.innerHTML = html;
+    Array.from(f.getElementsByTagName("link")).forEach((tag) => {
+      if (tag.rel == "stylesheet" && !tag.media) {
+        CMS.registerPreviewStyle(tag.href);
+      }
     });
+  });
