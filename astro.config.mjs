7cc557d23a1184283461b3b0608adced1b8fde18
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-01-30 19:24:24 +0000

    remove comments from html

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,20 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
-
   integrations: [
     sitemap(),
     prefetch(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
+    tailwind(),
+    mdx(),
+    compress({
+      html: {
+        removeComments: true,
       },
     }),
-    mdx(),
   ],
 });
