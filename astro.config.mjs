b20b1499d3cadfe4926c2b07af5cdfcbb597961c
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-01-30 16:32:29 +0000

    simplify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,18 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
-  markdown: {
-    remarkPlugins: [
-      "remark-html",
-      "remark-gfm",
-      [
-        "remark-footnotes",
-        {
-          inlineNotes: true,
-        },
-      ],
-    ],
-  },
+
   integrations: [
     sitemap(),
     prefetch(),
