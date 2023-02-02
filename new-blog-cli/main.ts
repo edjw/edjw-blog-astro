6d933ad5c1c970f566448c514a6b0a1195ba6768
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-02-02 23:37:11 +0000

    few improvements to the new blog post scaffolder

--- new-blog-cli/main.ts
+++ new-blog-cli/main.ts
@@ -1,3 +1,5 @@
+import slugify from "npm:slugify";
+
 const title = prompt("Add your title", "My Blog Post");
 const socialDescription = prompt(
   "Add a social description",
@@ -5,19 +7,27 @@ const socialDescription = prompt(
 );
 const tags = prompt("Add any tags (lower and kebab case)", "my-tag, my-tag");
 
+const slugifiedTitle = slugify(title, { lower: true, strict: true });
+
 const tagsArray = tags && tags.split(",").map((tag) => tag.trim());
 
 const featured = prompt("Feature on front page? (Y/N)", "N");
 
+const datetime = new Date().toISOString();
+
 const date = new Date().toISOString().split("T")[0];
 
-const data = `---
+const frontmatter = `---
 title: ${title}
-pubDate: ${date}
+pubDate: ${datetime}
 ${socialDescription && `socialDescription: ${socialDescription}`}
 ${tagsArray && "tags:"}
 ${tagsArray && tagsArray.map((tag) => `  - ${tag}`).join("\n")}
-${featured === "Y" ? "featured: true" : "featured: false"}
+${
+  featured && featured.toLowerCase() === "y"
+    ? "featured: true"
+    : "featured: false"
+}
 ---
 
 
@@ -25,7 +35,7 @@ ${featured === "Y" ? "featured: true" : "featured: false"}
 
 if (title) {
   await Deno.writeTextFile(
-    `./src/content/blog/${date}-${title.replace(/\s/g, "-").toLowerCase()}.md`,
-    data
+    `./src/content/blog/${date}-${slugifiedTitle}.md`,
+    frontmatter
   );
 }
