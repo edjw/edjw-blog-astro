b828156a78d2a93001a3a7797e0a4bb0ae85ca29
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-09-01 12:09:04 +0100

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-import compress from "astro-compress";
+
 
 export default defineConfig({
   site: "https://edjohnsonwilliams.co.uk",
@@ -10,13 +10,5 @@ export default defineConfig({
     sitemap(),
     prefetch(),
     tailwind(),
-    compress({
-      html: {
-        removeComments: true,
-      },
-    }),
-  ],
-  experimental: {
-    viewTransitions: true
-  }
+  ]
 });
