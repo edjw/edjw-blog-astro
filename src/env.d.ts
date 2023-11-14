21cbcc80228fb8899424377095a10d728df9ef58
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-11-14 20:27:00 +0000

    remove emailme; use aliases

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_MY_EMAIL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
