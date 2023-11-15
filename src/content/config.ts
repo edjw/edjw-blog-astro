0bbc6ac21d1bf9eec2bb554d30bd7d9435cc82bd
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-11-15 22:26:58 +0000

    adding a new stuff section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { z, defineCollection } from "astro:content";
 const kebabCaseRegex = new RegExp(/^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/);
 const blogCollection = defineCollection({
+  type: "content",
   schema: z
     .object({
       title: z.string().min(1),
@@ -20,6 +21,20 @@ const blogCollection = defineCollection({
     .strict(),
 });
 
+const StuffSchema = z
+  .object({
+    name: z.string(),
+    url: z.optional(z.string().url()),
+    description: z.string(),
+  })
+  .strict();
+
+const stuffCollection = defineCollection({
+  type: "data",
+  schema: z.array(StuffSchema),
+});
+
 export const collections = {
   blog: blogCollection,
+  stuff: stuffCollection,
 };
