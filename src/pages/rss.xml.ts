ac43596c022d12a89c9384fedc8a85b71b8b108a
Ed Johnson-Williams <edjw@users.noreply.github.com>
2026-01-03 21:50:32 +0000

    Enable CORS on RSS feed (#31)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,7 +9,7 @@ const parser = new MarkdownIt();
 
 export async function GET(context: { site: any }) {
   const blog: CollectionEntry<"blog">[] = await getCollection("blog");
-  return rss({
+  const response = await rss({
     title: siteData.title,
     description: siteData.description,
     site: context.site,
@@ -21,4 +21,11 @@ export async function GET(context: { site: any }) {
       content: sanitizeHtml(parser.render(post.body || "")),
     })),
   });
+
+  // Add CORS headers to allow cross-origin requests
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  return response;
 }
